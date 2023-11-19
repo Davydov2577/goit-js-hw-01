7a218ff4e343b4372fdf45960b52709837ef7824
Aleksandr Davydov <113263675+Davydov2577@users.noreply.github.com>
2023-11-19 11:56:57 +0200

    task-2-upgrd

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function getShippingMessage(country, price, deliveryFee) {
-  let totalPrice = price * deliveryFee;
+  let totalPrice = price + deliveryFee;
   return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
